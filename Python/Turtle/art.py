70a95847eb86f95909c3d1c7d09cdc38fd01f0af
MetalDent <priyapandya274@gmail.com>
2019-05-25 23:26:05 +0530

    Add python code

--- Python/Turtle/art.py
+++ Python/Turtle/art.py
@@ -23,5 +23,36 @@ for i in range(1, 30):
   randomheading()
   stamp()
 
+speed(0)
+clear()
+
+def drawcircle():
+  radius = randint(5, 100)
+  randomcolour()
+  randomplace()
+  dot(radius)
+  
+def drawstar():
+  randomcolour()
+  randomplace()
+  randomheading()
+  begin_fill()
+  size = randint(20, 100)
+  for side in range(5):
+    left(144)
+    forward(size)
+
+  end_fill()
+  
+clear()
+setheading(0)
+
+for i in range(50):
+  drawcircle()
+
+clear()
+
+for i in range(20):
+  drawstar()
 
 done() 
