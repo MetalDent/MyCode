a97ee13b4e04cdeca7a7a777e0dd3d3577a2c7c4
MetalDent <priyapandya274@gmail.com>
2019-06-07 21:32:10 +0530

    Add code

--- Python/email_sender/email_sender.py
+++ Python/email_sender/email_sender.py
@@ -1,18 +1,12 @@
 import smtplib
-import sqlite3
-
 from email.mime.text import MIMEText
-
-conn = sqlite3.connect('recipients.db')
-cursor = conn.cursor()
-
 
 msg = MIMEText("""Hello""")
 s = smtplib.SMTP('Smtp.gmail.com')
 s.starttls()
 s.login('username','password')
 sender = 'priyapandya274@gmail.com'
-recipients = cursor.execute("select * from sheet1;")
+recipients = ['email1', 'email2']
 msg['Subject'] = "Test"
 msg['From'] = sender
 msg['To'] = ", ".join(recipients)
