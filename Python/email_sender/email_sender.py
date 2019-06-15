80f6c007d8cd88866ba669a976d4fd1af9660707
MetalDent <priyapandya274@gmail.com>
2019-06-16 00:18:29 +0530

    Add code

--- Python/email_sender/email_sender.py
+++ Python/email_sender/email_sender.py
@@ -14,19 +14,19 @@ s.login('username','password')	#login credentials
 sender = 'priyapandya274@gmail.com'	#sender email
 recipients = []	#recepients from database
 
-file_location = '/media/metaldent/Study_Coding/Programming/MyCode/Python/websraping/image.jpg'
+file_location = '/path/to/file'	#path of the attached file
 
 
 with open('recipients.txt', 'r') as filehandle:	#get recipients from the text file
     recipients = json.load(filehandle)
 
+msg = MIMEMultipart()
+
 msg['Subject'] = "Test"	#subject
 msg['From'] = sender	#from
 msg['To'] = ", ".join(recipients)	#to
 
-msg.attach(MIMEText(msg, 'plain'))
-
-# Setup the attachment
+#Setup the attachment
 filename = os.path.basename(file_location)
 attachment = open(file_location, "rb")
 part = MIMEBase('application', 'octet-stream')
@@ -34,8 +34,7 @@ part.set_payload(attachment.read())
 encoders.encode_base64(part)
 part.add_header('Content-Disposition', "attachment; filename= %s" % filename)
 
-# Attach the attachment to the MIMEMultipart object
-msg.attach(part)
+msg.attach(part)	#Attach the attachment to the MIMEMultipart object
 
 s.sendmail(sender, recipients, msg.as_string())
 s.quit()
