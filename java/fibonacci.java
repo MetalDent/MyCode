926891f2dab26c5664f5dfbee54419210d6ef250
MetalDent <priyapandya274@gmail.com>
2019-08-22 09:52:11 +0530

    Update

--- java/fibonacci.java
+++ java/fibonacci.java
@@ -11,18 +11,20 @@ public class fibonacci {
 		System.out.println("Enter the no of terms: ");
 		
 		int n = input.nextInt();
+
+		System.out.println("Fibonacci numbers: ");
 		
 		for(int i = 0 ; i < n ; i++) {
 			
 			if(i == 0)
-				System.out.printf("%d ", f);
+				System.out.printf("%d \n", f);
 			else if(i == 1)
-				System.out.printf("%d ", s);
+				System.out.printf("%d \n", s);
 			else {
 				
 				next = f + s;
 				
-				System.out.printf("%d ", next);
+				System.out.printf("%d \n", next);
 				
 				f = s;
 				s = next;
