6e239f677eeec70f9e0169a47b3a95e9ed6fd65e
MetalDent <priyapandya274@gmail.com>
2019-09-11 17:46:55 +0530

    Add code: Django

---
+++ Django/py_venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/metaldent/Study_Coding1/Programming/MyCode/Django/py_venv/bin/python3
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
